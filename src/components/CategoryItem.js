7e43a5d032d75f179b057f5384202d934321d98d
Pablo Espinosa <pabloesp2003@gmail.com>
2025-05-16 19:57:33 -0600

    FinalResponsive

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -3,14 +3,16 @@ import React from "react";
 
 export default function CategoryItem({ categories, selected, onSelect }) {
     return (
-        <div className="ctgrs" style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+        <div className="categories2">
             <h2 style={{ marginBottom: "1rem", fontSize: "1.2rem" }}>Categories</h2>
-            {categories.map(ctg => (
-                <button key={ctg.idCategory} onClick={() => onSelect(ctg.strCategory)} style={{ backgroundColor: "#0f0f2d", border: "2px solid #1e1e3f", color: "white", padding:"0.3rem", borderRadius:"var(--border-radius)", cursor: "pointer", display: "flex", alignItems: "center", gap: "0.8rem", fontSize: "1rem"}}>
-                    <img src={ctg.strCategoryThumb} alt={ctg.strCategory} style={{ width: "30px", height: "30px", objectFit: "cover", borderRadius: "50%" }}/>
-                    {ctg.strCategory}
-                </button>
-            ))}
+            <div className="ctgrs">
+                {categories.map(ctg => (
+                    <button key={ctg.idCategory} onClick={() => onSelect(ctg.strCategory)} style={{ backgroundColor: "#0f0f2d", border: "2px solid #1e1e3f", color: "white", padding:"0.3rem", borderRadius:"var(--border-radius)", cursor: "pointer", display: "flex", alignItems: "center", gap: "0.8rem", fontSize: "1rem"}}>
+                        <img src={ctg.strCategoryThumb} alt={ctg.strCategory} style={{ width: "50px", height: "30px", objectFit: "cover", borderRadius: "50%" }}/>
+                        {ctg.strCategory}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 }
